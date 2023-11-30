afd54cc057ac09b0fd19e18b9a91735242c72b71
Andrea Ridolfi <ridolfi.a20@gmail.com>
2023-11-30 17:04:42 +0100

    added new example files and cleaned the indentation_fit from nanoscope..CurveFit.py

--- Nanoscope_converter.py
+++ Nanoscope_converter.py
@@ -1,4 +1,6 @@
 import re
+
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import itertools
@@ -7,134 +9,112 @@ from scipy.signal import savgol_filter # Savitzky Golay filter for smoothing dat
 import datetime
 
 def nanoscope_converter(file_path):
+    '''Parser for a force curve raw file. Takes the path to the file as input and outputs all relevant information
+    about a curve'''
+
+    # Read the curve file as a binary
     with open(file_path, 'rb') as path:
-        path.seek(40960) # where the binary data begins
-        # testo = str(path.read()) # useless
+        # first part of the file is a header, put the index where the binary data starts
+        path.seek(40960)
+        # converting to integer values these data are in the format called "LeastSignificantBit" (LSB) and refer to
+        # the deflection signal of both approach and retraction curves
         all_deflections = np.fromfile(path, dtype=np.int16)
-        # ha trovato i valori in LSB delle deflection sia approach che retraction infatti è lunga 8192
+        # extracting header info from first part
         path.seek(0)
         header_info = str(path.read())
 
+    '''The header contains some info and values necessary for calculating the force curves, this info needs to be 
+    parsed and extracted.'''
     # find z sensitivity in nm/V
     index = header_info.find('@Sens. Zsens: V')
     sensitivity = float(re.findall("\d+\.\d+", header_info[index:])[0])
-
     # find the size of the probed area and the x and y position of the curve
     index = header_info.find('\Scan Size: ')
     scan_size = float(re.findall("\d+", header_info[index:])[0])
+    # indentation x position (nm)
     index = header_info.find('\X Offset: ')
-    x_pos = float(re.findall("\d+", header_info[index:])[0])   # x position in nm (* -1 is for displaying them correctly)
+    x_pos = float(re.findall("\d+", header_info[index:])[0])
+    # indentation y position (nm)
     index = header_info.find('\Y Offset: ')
-    y_pos = float(re.findall("\d+", header_info[index:])[0])   # y position in nm
-
-    # find the indentation rate in Hz
+    y_pos = float(re.findall("\d+", header_info[index:])[0])
+    # indentation rate in Hz
     index = header_info.find('\Scan rate: ')
     ind_rate = float(re.findall("\d+\.\d+", header_info[index:])[0])
-
     # find the forward velocity in nm/s
     index = header_info.find('\Forward vel.: ')
     forw_vel = float(re.findall("\d+\.\d+", header_info[index:])[0])*100
-
+    # Ramp size (V), the interesting value is the second after @4:Ramp size..
     index = header_info.find('@4:Ramp size Zsweep: V')
-    ramp_size_in_Volt = float(re.findall("\d+\.\d+", header_info[index:])[1]) # interested value is the second after @4:Ramp size..
+    ramp_size_in_Volt = float(re.findall("\d+\.\d+", header_info[index:])[1])
     ramp_size_in_nm = sensitivity*ramp_size_in_Volt
-
+    # n° of points in the retraction curve
     index = header_info.find('Samps/line: ', index)
-    Rt_npoints = int(re.findall("\d+", header_info[index:])[0]) # n° of points in the retraction curve
-    Ex_npoints = int(re.findall("\d+", header_info[index:])[1]) # n° of points in the approach curve
-    Rt_step = ramp_size_in_nm/Rt_npoints # step increment along the retraction ramp
-    Ex_step = ramp_size_in_nm/Ex_npoints # step increment along the approach ramp
+    Rt_npoints = int(re.findall("\d+", header_info[index:])[0])
+    # n° of points in the approach curve
+    Ex_npoints = int(re.findall("\d+", header_info[index:])[1])
 
-    # building up the approach ramp array
-    Ex_ramp = np.array(0)
-    new, i = 0, 0
-    while i < Ex_npoints-1:
-        new = new + Ex_step
-        Ex_ramp = np.append(Ex_ramp, new)
-        i += 1
+    '''The binary data only contains the info about the cantilever deflection, the height (ramp) values need to be 
+    computed from the data of the header.'''
+    # step increment along the retraction ramp
+    Rt_step = ramp_size_in_nm/Rt_npoints
+    # step increment along the approach ramp
+    Ex_step = ramp_size_in_nm/Ex_npoints
+    # Approach ramp array
+    Ex_ramp = np.zeros(Ex_npoints)
+    for i in range(1, Ex_npoints, 1):
+        Ex_ramp[i] = Ex_ramp[i-1] + Ex_step
+    # Retraction ramp array (should be in the reverse order)
+    Rt_ramp = np.full(Rt_npoints, ramp_size_in_nm)
+    for i in range(1, Rt_npoints, 1):
+        Rt_ramp[i] = Rt_ramp[i - 1] - Rt_step
 
-    # building up the retraction ramp array
-    Rt_ramp = np.array(ramp_size_in_nm)
-    i, new = 0, ramp_size_in_nm
-    while i < Rt_npoints-1:
-        new = new - Rt_step
-        Rt_ramp = np.append(Rt_ramp, new)
-        i += 1
-
-    # building up the APPROACH DEFLECTION ARRAY
+    # Approach deflections array
     Ex_deflection = all_deflections[0:Ex_npoints]
     index = header_info.find('@4:Z scale:')
     from_LSB_to_Volt = float(re.findall("\d+\.\d+", header_info[index:])[0])
     # find the spring constant nN/nm
     index = header_info.find('Spring Constant:')
     spring_constant = float(re.findall("\d+\.\d+", header_info[index:])[0])
-    #find the Deflection sensitivity  nm/V
+    #find the Deflection sensitivity nm/V
     index = header_info.find('@Sens. DeflSens: V')
     defl_sensitivity = float(re.findall("\d+\.\d+", header_info[index:])[0])
-    # deflection in nm and force in pN arrays for approach
+    # Approach deflections in nm
     Ex_deflection = Ex_deflection*from_LSB_to_Volt*defl_sensitivity
-    Ex_deflection = np.flip(Ex_deflection) # inverting the array, it's in the wrong sense
+    # from the raw file the array is reversed wrt the ramp array so needs to be flipped
+    Ex_deflection = np.flip(Ex_deflection)
+    # Approach force in pN
     Ex_force = Ex_deflection*spring_constant*1000
 
-
-    # building up the RETRACTION DEFLECTION ARRAY
+    # Retraction deflections array
     Rt_deflection = all_deflections[Rt_npoints:2*Rt_npoints]
-    # deflection in nm and force in pN arrays for retraction
+    # Retraction deflections in nm
     Rt_deflection = Rt_deflection*from_LSB_to_Volt*defl_sensitivity
+    # from the raw file the array is reversed wrt the ramp array so needs to be flipped
+    Rt_deflection = np.flip(Rt_deflection)
+    # Retraction force in pN
     Rt_force = Rt_deflection*spring_constant*1000
 
-    #------SAVITZKY GOLAY FILTERING IN THE CASE THE SIGNAL PRESENTS TOO MUCH NOISE
+    # Savitzky Golay filter to reduce noise
     Ex_deflection_filtered = savgol_filter(Ex_deflection, 7, 3)
     Rt_deflection_filtered = savgol_filter(Rt_deflection, 7, 3)
-    # Ex_deflection_filtered = Ex_deflection
-    # Rt_deflection_filtered = Rt_deflection
-
+    # Forces filtered
     Ex_force_filtered = Ex_deflection_filtered*spring_constant*1000
     Rt_force_filtered = Rt_deflection_filtered*spring_constant*1000
-    Rt_force = np.flip(Rt_force)
-    Rt_force_filtered = np.flip(Rt_force_filtered)
 
+    # Estimating the baseline in order to shift to zero the initial force values
+    Ex_force_shifted = Ex_force - np.mean(Ex_force[:1000])
+    Rt_force_shifted = Rt_force - np.mean(Rt_force[:1000])
+    Ex_force_filtered = Ex_force_filtered - np.mean(Ex_force_filtered[:1000])
+    Rt_force_filtered = Rt_force_filtered - np.mean(Rt_force_filtered[:1000])
 
-    # calculating the baseline in order to shift to zero the initial force values
-    sum = 0
-    Rt_sum = 0
-    for i in range(1000):
-        sum = sum + Ex_force[i]
-        Rt_sum = Rt_sum + Rt_force[i]
-
-    Ex_force_shifted = Ex_force - sum/1000
-    Rt_force_shifted = Rt_force - Rt_sum/1000
-
-    Ex_force_filtered = Ex_force_filtered - sum/1000
-    Rt_force_filtered = Rt_force_filtered - Rt_sum/1000
-
-
-    # EXPOPORTING A TEXT FILE OF THE RAW DATA
-    # newpath = str(file_path) + '_rawCurveInfo.txt'
-    # with open(newpath, 'w') as text:
-    #     text.write("Calc_Ramp_Ex_nm     Calc_Ramp_Rt_nm     Defl_nm_Ex     Defl_nm_Rt     Defl_pN_Ex     Defl_pN_Rt\n")
-    #     for xyz in itertools.zip_longest(Ex_ramp, Rt_ramp, Ex_deflection, Rt_deflection, Ex_force, Rt_force, fillvalue=''):
-    #         text.write('%-20s   %-20s   %-20s   %-20s   %-20s   %-20s\n'%xyz)
-    # #exporting a text file of the separation vs Force both Ex and Rt
-    # newpath2 = str(file_path) + '_Ex_ForceSeparation.txt'
-    # newpath3 = str(file_path) + '_Rt_ForceSeparation.txt'
-    # with open(newpath2, 'w') as text:
-    #     text.write("Separation_Ex_nm     Force_Ex_pN\n")
-    #     for xyz in itertools.zip_longest(Ex_ramp-Ex_deflection,Ex_force, fillvalue=''):
-    #         text.write('%-20s   %-20s\n'%xyz)
-    #
-    # with open(newpath3, 'w') as text:
-    #     text.write("Separation_Rt_nm     Force_Rt_pN\n")
-    #     for xyz in itertools.zip_longest(Rt_ramp-Rt_deflection,Ex_force, fillvalue=''):
-    #         text.write('%-20s   %-20s\n'%xyz)
-
+    # Separation arrays are built by subtracting deflection form the height
     Ex_separation = np.array(Ex_ramp-Ex_deflection)
-    Rt_separation = np.array(Rt_ramp-Rt_deflection)
-
+    Rt_separation = np.array(Rt_ramp+Rt_deflection)
     Ex_separation_filtered = np.array(Ex_ramp-Ex_deflection_filtered)
-    Rt_separation_filtered = np.array(Rt_ramp-Rt_deflection_filtered)
+    Rt_separation_filtered = np.array(Rt_ramp+Rt_deflection_filtered)
 
 
+    # Setting the "surface" as zero of the curve
     Ex_max = np.max(Ex_separation)
     aligned_Ex_sep = np.array((Ex_separation - Ex_max)*-1)
     Rt_max = np.max(Rt_separation)
@@ -150,5 +130,14 @@ def nanoscope_converter(file_path):
              aligned_Ex_sep_filtered, Ex_force_filtered, aligned_Rt_sep_filtered, Rt_force_filtered,
              forw_vel, ind_rate, scan_size, x_pos, y_pos]
 
+    # plt.plot(aligned_Ex_sep, Ex_force_shifted, 'b', zorder=1)
+    # plt.plot(aligned_Rt_sep, Rt_force_shifted, 'r', zorder=2)
+    # plt.grid()
+    # plt.show()
+
 
     return curve
+
+
+nanoscope_converter("cr00028.000")
+# nanoscope_converter("DPPC-DSPC_100times00154.021")
--- nanoscope_ChromosomeCurveFit.py
+++ nanoscope_ChromosomeCurveFit.py
@@ -1,101 +1,81 @@
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from matplotlib.colors import LogNorm
 from Nanoscope_converter import nanoscope_converter
 import numpy as np
-import pandas as pd
-#import seaborn as sns
-from scipy.optimize import curve_fit, leastsq
-import os.path
+from scipy.optimize import curve_fit
 from numpy.linalg import norm
 from scipy.signal import savgol_filter  # Savitzky Golay filter for smoothing data with too much noise
-import glob  # this is for listing all the files in a given directory
-from nanoscope_CurvesContactPoint_determination import contact_pointFinder1, contact_pointFinder2
+from nanoscope_CurvesContactPoint_determination import contact_pointFinder2
 from scipy.integrate import simps
 
 
 
-def fit_func_linear(x, a, b):  # sensitivity is corrected by applying a linear fit to the last part of the curve
+# function for linear fit
+def fit_func_linear(x, a, b):
     return a * x + b
 
+
 def indentation_fit(path):
-    # ---- HOW MANY POINTS TO USE FOR CORRECTING THE SENSITIVITY? ----
-    # points = input('How many points to use for correcting the sensitivity? Type "no" for avoid sensitivity correction ')
-    # points = 100
-    points = 'no'
-    curves_name = []
-    Young_modulus = []
-    Young_modulus_TipC = []
-    all_curves = []
-    cpoints = []
-    ALL_forw_vel = []
-    ALL_ind_rate = []
-    RSE_values = []
-    RSE_values_TipC = []
-    ALL_x_pos = []
-    ALL_y_pos = []
-    ALL_fit_sep = []
-    ALL_fit = []
-    ALL_fit_sep_TipC = []
-    ALL_fit_TipC = []
+    '''Fit the approach curve with different contact mechanics models'''
+    # Reading the curve data
     curve_data = nanoscope_converter(path)
-    raw_separation = curve_data[0]  # script per lettura e conversione dei file Bruker
-    force = curve_data[1]  # script per lettura e conversione dei file Bruker
-    forw_vel = curve_data[10]  # forward indenting velocity in nm/s
-    ALL_forw_vel.append(forw_vel)
-    ind_rate = curve_data[11]  # indentation rate in Hz
-    ALL_ind_rate.append(ind_rate)
-    scan_size = curve_data[12]  # scan size
+    raw_separation = curve_data[0]
+    force = curve_data[1]
+    # forw_vel = curve_data[10]
+    # ind_rate = curve_data[11]
+    # scan_size = curve_data[12]
     x_pos = curve_data[13]
     y_pos = curve_data[14]
-    ALL_x_pos.append(x_pos)
-    ALL_y_pos.append(y_pos)
-    # -------CORRECTION FOR SENSITIVITY-------
+    # In the case the sensitivity was not correctly calibrated in the instrument
+    # fit the part of curve describing surface contact and puts it vertical straight
+    # points = 100 # n. of points to use for the fit
+    points = 'no'
     if points == 'no':
         separation = raw_separation
     else:
         points = int(points)
         corrected_separation = raw_separation[(len(raw_separation) - points):]
         corrected_force = force[(len(force) - points):]
-        fit_param = curve_fit(fit_func_linear, corrected_separation, corrected_force, maxfev=100000)
-        correction = (force - fit_param[0][1]) / fit_param[0][0]
+        fit_param, pcov = curve_fit(fit_func_linear, corrected_separation, corrected_force, maxfev=100000)
+        correction = (force - fit_param[1]) / fit_param[0]
         separation = raw_separation - correction
-    # ---- CORRECTION FOR BASELINE NOT HORIZONTAL ----
+    # Sometimes (due to instrumental errors) both approaching and
+    # retracting curves are not perfectly orizontal horizontal, correction is similar to sensitivity
+    # points to use to level the curves (first # points in the baseline part)
     n = 1000
-    new_sep = raw_separation[:n]
-    new_force = force[:n]
-    fit_param = curve_fit(fit_func_linear, new_sep, new_force, maxfev=100000)
-    force = force - (fit_param[0][0] * raw_separation + fit_param[0][1])
-    # ------SAVITSKY GOLAY FILTERING IN THE CASE THE SIGNAL PRESENTS TOO MUCH NOISE
+    fit_param, pcov = curve_fit(fit_func_linear, raw_separation[:n], force[:n], maxfev=100000)
+    force = force - (fit_param[0] * raw_separation + fit_param[1])
+    # Further filtering when the signal is particularly noisy
     force = savgol_filter(force, 35, 3, mode='nearest')
-    # ---- CONTACT POINT DETERMINATION ----
+    # Contact point determination
     contact = contact_pointFinder2(separation, force)
     cp_x, cp_y, cp_index = contact[0], contact[1], contact[2]
-    cpoints.append(cp_x)
     force = force - cp_y
-    # Calculate the area below the indentation curve
-    area_simps = simps(force, dx=np.abs(separation[1] - separation[0]))
-    # Shifting the y coordinate of the contact point to 0 (and shifting up the whole curve) in order
-    # not to compromise the fit when the force is below zero due to instabilities of the baseline
-    # ---- FITTING OF THE DELIMITED PART OF THE CURVE ----
-    # define the part of the curve to be fitted (subtracting cp_x because the x coordinate
-    # should be zero at the contact pt., i.e. when the fit starts)
-    # fitting a window that is n% of the total indentation (cp_x - n/100 * cp_x)
-    end_pt = [i for i in range(len(separation)) if separation[i] < cp_x - 0.3 * cp_x]
+    # Fitting: data preparation
+    # should be zero at the contact pt., i.e. when the fit starts) setting the contact point ascissa as zero and
+    # fitting a portion of curve n times the total indentation (cp_x - n * cp_x)
+    n = .3
+    # index of last point of the fitted section
+    end_pt = [i for i in range(len(separation)) if separation[i] < cp_x - n * cp_x]
+    # portion of curve to be fitted
     fit_separation = separation[cp_index: end_pt[0]] - cp_x
     fit_force = force[cp_index: end_pt[0]]
-    R_tip = 10  # tip radius--> in future better to insert as input request
-    nu = 0.5  # Poisson coefficient -->  either 0.5 or 0.7
-    t = cp_x  # thickness in nm --> set equal to the contact point x coordinate
+    # Properties of the tip (only used in Hertz models)
+    # tip radius (nm)
+    R_tip = 10
+    # Poisson coefficient
+    nu = 0.5
+    # thickness of the sample (nm), set equal to the contact point x coordinate
+    t = cp_x
 
-    # ---- Standard Hertz fit model ----
+    # Fitting functions:
     def fit_func_hertz(x, E):
+        '''Classical Hertz fit model.'''
         f = (4 / 3) * (R_tip ** 0.5) * (E / (1 - (nu) ** 2)) * (np.abs(x) ** 1.5)
         return f
 
-    # ---- Modified Hertz fit (from Dimitriadis et al. Biophysical journal 82.5 (2002): 2798-2810.) ----
-    # #      FOR SAMPLES NOT BONDED TO THE SURFACE WHICH CAN SLIP (E is in MPa)
     def fit_func_modifhertz_nb(x, E):
+        '''Modified Hertz fit from Dimitriadis et al. Biophysical journal 82.5 (2002): 2798-2810.
+        FOR SAMPLES NOT BONDED TO THE SURFACE WHICH CAN SLIP (E is in MPa).'''
         f = (16 / 9) * E * ((R_tip) ** 0.5) * (np.abs(x) ** 1.5) * ((
                 1 + 0.884 * ((R_tip * np.abs(x)) ** 0.5) / t + 0.781 * (
                 ((R_tip * np.abs(x)) ** 0.5) / t) ** 2 + 0.386 * (
@@ -103,9 +83,9 @@ def indentation_fit(path):
                         ((R_tip * np.abs(x)) ** 0.5) / t) ** 4))
         return f
 
-    # ---- (Modified Hertz fit from Dimitriadis et al. Biophysical journal 82.5 (2002): 2798-2810.) ----
-    # #      FOR SAMPLES BONDED TO THE SURFACE WHICH CANNOT SLIP (E is in MPa)
     def fit_func_modifhertz_b(x, E):
+        '''Modified Hertz fit from Dimitriadis et al. Biophysical journal 82.5 (2002): 2798-2810.
+        FOR SAMPLES BONDED TO THE SURFACE WHICH CANNOT SLIP (E is in MPa).'''
         f = (16 / 9) * E * ((R_tip) ** 0.5) * (np.abs(x) ** 1.5) * ((
                 1 + 1.133 * ((R_tip * np.abs(x)) ** 0.5) / t + 1.283 * (
                 ((R_tip * np.abs(x)) ** 0.5) / t) ** 2 + 0.769 * (
@@ -113,83 +93,59 @@ def indentation_fit(path):
                         ((R_tip * np.abs(x)) ** 0.5) / t) ** 4))
         return f
 
-    # ---- Corrected fit for conical tips (from Garcia R. et al. Biophysical journal 114.12 (2018): 2923-2932) ----
-    #  h is the sample thickness (contact point) and theta is the cone angle
     def fit_func_garcia_conicalTip(x, E):
+        '''Corrected fit for conical tips (from Garcia R. et al. Biophysical journal 114.12 (2018): 2923-2932). h is
+        the sample thickness (contact point) and theta is the cone angle.'''
         theta = np.deg2rad(35)
         f0 = (8 * np.tan(theta) * E * x ** 2) / (3 * np.pi)
         f = f0 * (1 + (0.721 * x * np.tan(theta) / t) + (0.650 * x ** 2 * (np.tan(theta)) ** 2 / t ** 2) + (
-                    0.491 * x ** 3 * (np.tan(theta)) ** 3 / t ** 3) + (
-                              0.225 * x ** 4 * (np.tan(theta)) ** 4 / t ** 4))
+                0.491 * x ** 3 * (np.tan(theta)) ** 3 / t ** 3) + (
+                          0.225 * x ** 4 * (np.tan(theta)) ** 4 / t ** 4))
         return f
 
-    # simple exponential function
-    def fit_HertzEffectiveCP(x, E):
-        params, pcov = curve_fit(selected_fit, fit_separation, fit_force, p0=[0.01], bounds=(0, 10000),
-                                  maxfev=100000000)
-        fit = selected_fit(fit_separation, params[0])
-        # calculate residuals
-        res = fit_force - fit
-        sum_res = np.sum(res ** 2)
-        print(sum_res)
-        print("E= ", params[0]*1000)
 
-
-
-    # ---- FITTING THE INDENTATION CURVE ----
+    # Applying the fit to the indentation portion
     # (if wants to fit with moving window, see end of the script)
     fitting_models = [fit_func_hertz, fit_func_modifhertz_nb, fit_func_modifhertz_b,
-                      fit_func_garcia_conicalTip, fit_HertzEffectiveCP]
-    # ---- SELECT THE FITTING MODEL -----
+                      fit_func_garcia_conicalTip]
+    # Selecting which model to use for the fit
     selected_fit = fitting_models[2]
-    E_bis = (15 / 8) * (1 - nu ** 2) * (cp_x ** (-5 / 2)) * area_simps / (np.sqrt(R_tip))
-    params, pcov = curve_fit(selected_fit, fit_separation, fit_force, p0=[0.001], bounds=(0, 10000),
-                             maxfev=100000000)
-    fit = selected_fit(fit_separation, params[0])
+    params, pcov = curve_fit(selected_fit, fit_separation, fit_force, p0=[0.001],
+                             bounds=(0, 10000), maxfev=100000000)
+    fit = selected_fit(fit_separation, *params)
+    # Young Modulus in kPa
     young_modulus = params[0] * 1000
     try:
-        # params, pcov = curve_fit(selected_fit, fit_separation, fit_force)
-        # fit = selected_fit(fit_separation, params[0], params[1])
-        # Young modulus in kPa
-        # print('Young Modulus (kPa): ', young_modulus)
         # R square, taken from Lin, David C., et al. "Spherical indentation of soft matter beyond the Hertzian regime:
         # numerical and experimental validation of hyperelastic models." Biomechanics and modeling in mechanobiology 8.5
         # (2009): 345-358
         error = 1 - (norm(fit_force - fit) ** 2 / norm(fit_force - np.mean(fit_force)) ** 2)
         # print('R square: ', R2)
     except:
-        # fit = [0]*len(fit_separation)
-        # young_modulus = 0
+        # when the fit does not converge (or is bad) r-squared cannot be calculated correctly, in these cases its
+        # value is set to 0 to be discarded later
         error = 0
-        # print('Fit error')
+    # Calculate Young Modulus from the area under the indenting curve
+    area_simps = simps(force, dx=np.abs(separation[1] - separation[0]))
+    # the area under the indenting curve (indentation energy) is equal to the integral of the force(described by
+    # Hertz equation). From that we extract the Young Modulus as E_bis
+    E_bis = (15 / 8) * (1 - nu ** 2) * (cp_x ** (-5 / 2)) * area_simps / (np.sqrt(R_tip))
 
-    # params, pcov = curve_fit(selected_fit, fit_separation, fit_force, p0=[0.01], bounds=(0, 10000), maxfev=100000000)
-    # fit = selected_fit(fit_separation, params[0])
-    # Young modulus in kPa
-    # young_modulus = params[0]*1000
-    #print('Young Modulus (kPa): ', young_modulus)
-    # print('Young modulus: %f Covariance: %f ' % (young_modulus, std_dev))
+    # Plotting the curve
+    if error == 0:
+        print(error)
+    else:
+        fig, ax = plt.subplots()
+        ax.grid()
+        ax.scatter(separation, force, s=1, c='gray', alpha=0.5)
+        ax.scatter(cp_x, cp_y, s=25, c='k', marker='X')
+        ax.set_xlabel('Separation (nm)')
+        ax.set_ylabel('Force (pN)')
+        ax.plot(fit_separation + cp_x, fit, c='r')
+        plt.show()
 
-    # ---- PLOTTING ----
-    # plt.ion()
-    # if error < 0:
-    #     print(error)
-    #     fig, ax = plt.subplots()
-    #     ax.grid()
-    #     ax.scatter(separation, force, s=1, c='gray', alpha=0.5)
-    #     ax.scatter(cp_x, cp_y, s=25, c='k', marker='X')
-    #     ax.set_xlabel('Separation (nm)')
-    #     ax.set_ylabel('Force (pN)')
-    #     # ---- add cp_x to shift the fit on the old curve where zero is the surface ----
-    #     ax.plot(fit_separation + cp_x, fit, c='r')
-    #     plt.show()
-    # ax.plot(fit_separation + cp_x, fit + RSE, c='g')
-    # ax.plot(fit_separation + cp_x, fit - RSE, c='g')
-    # plt.show()
-    # plt.pause(0.7)
-    # plt.close()
-    # print(type(x_pos), type(error))
     return x_pos, y_pos, young_modulus, error, cp_x, area_simps, E_bis
+
 
 def area_viscoelasticity(path):
     # ---- HOW MANY POINTS TO USE FOR CORRECTING THE SENSITIVITY? ----
@@ -224,11 +180,16 @@ def area_viscoelasticity(path):
         correction = (force - fit_param[0][1]) / fit_param[0][0]
         separation = raw_separation - correction
     # ---- CORRECTION FOR BASELINE NOT HORIZONTAL ----
-    n = 1000
+    n = 1500
     new_sep = raw_separation[:n]
     new_force = force[:n]
     fit_param = curve_fit(fit_func_linear, new_sep, new_force, maxfev=100000)
     force = force - (fit_param[0][0] * raw_separation + fit_param[0][1])
+
+    # rt_new_sep = rt_separation[:n]
+    # rt_new_force = rt_force[:n]
+    # fit_param = curve_fit(fit_func_linear, rt_new_sep, rt_new_force, maxfev=100000)
+    # rt_force = rt_force - (fit_param[0][0] * rt_separation + fit_param[0][1])
     # ------SAVITSKY GOLAY FILTERING IN THE CASE THE SIGNAL PRESENTS TOO MUCH NOISE
     force = savgol_filter(force, 35, 3, mode='nearest')
     # ---- CONTACT POINT DETERMINATION ----
@@ -248,15 +209,17 @@ def area_viscoelasticity(path):
     a2 = rt_area_simps
     eta = 1 - (a2/a1)
     #
-    # fig, ax = plt.subplots()
-    # ax.grid()
-    # ax.scatter(separation, force, s=1, c='gray', alpha=0.5)
-    # ax.scatter(rt_separation, rt_force, s=1, c='gray', alpha=0.5)
-    # ax.scatter(cp_x, cp_y, s=25, c='k', marker='X')
-    # ax.scatter(rt_cp_x, rt_cp_y, s=25, c='orange', marker='X')
-    # ax.set_xlabel('Separation (nm)')
-    # ax.set_ylabel('Force (pN)')
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.scatter(separation, force, s=1, c='b', alpha=0.5)
+    ax.scatter(rt_separation, rt_force, s=1, c='r', alpha=0.5)
+    ax.scatter(cp_x, cp_y, s=25, c='k', marker='X')
+    ax.scatter(rt_cp_x, rt_cp_y, s=25, c='orange', marker='X')
+    ax.set_xlabel('Separation (nm)')
+    ax.set_ylabel('Force (pN)')
+    plt.show()
+
+    print(eta)
     return x_pos, y_pos, eta, cp_x
 
 
@@ -295,3 +258,7 @@ def area_viscoelasticity(path):
         # ax.plot(fit_separation + cp_x, fit + RSE, c='g')
         # ax.plot(fit_separation + cp_x, fit - RSE, c='g')
         plt.show() '''
+
+
+# area_viscoelasticity("cr00175.000")
+indentation_fit("cr00037.000")
