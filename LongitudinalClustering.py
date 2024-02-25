6bf4832fd1beae2d2e64717e88561ba3eab6503e
Andrea Ridolfi <ridolfi.a20@gmail.com>
2024-02-25 18:14:19 +0100

    edits to Longitudinal Clustering algorithm to perform either euclidean distance or Dynamic Time Warping (DTW) analysis. The script generates a .csv file containing the condensed distance matrix that can be used in other scripts for plotting and analyses.

--- LongitudinalClustering.py
+++ LongitudinalClustering.py
@@ -1,48 +1,44 @@
-import math
 import numpy as np
 import pandas as pd
-import scipy.cluster.hierarchy as hac
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import fastdtw
 from scipy.spatial.distance import squareform
 import time
-from joblib import Parallel, delayed
-import multiprocessing
 
 
-def plot_dendogram(Z):
+
+def plot_dendrogram(Z):
     with plt.style.context('fivethirtyeight' ):
          plt.figure(figsize=(15, 5))
          plt.title('Dendrogram for curve clustering', fontsize=25, fontweight='bold')
-         plt.xlabel('sample index', fontsize=25, fontweight='bold')
-         plt.ylabel('distance', fontsize=25, fontweight='bold')
-         hac.dendrogram(Z, leaf_rotation=90.,  # rotates the x axis labels
+         plt.xlabel('sample index', fontsize=15)
+         plt.ylabel('distance', fontsize=15)
+         dendrogram(Z, leaf_rotation=90.,  # rotates the x axis labels
                         leaf_font_size=15) # and font size for the x axis labels
          plt.show()
 
 def plot_results(yaxis, xaxis, D, cut_off_level):
-    result = pd.Series(hac.fcluster(D, cut_off_level, criterion='distance'))
+    # assign each curve to a specific cluster (based on cut_off_level)
+    result = pd.Series(fcluster(D, cut_off_level, criterion='distance'))
     clusters = result.unique()
-    print(result.value_counts())
     fig, ax1 = plt.subplots(nrows=1, ncols=len(clusters))
     fig, ax2 = plt.subplots()
-    palette = sns.color_palette("magma", len(clusters))
-    # palette = ['gray', 'darkred', 'blue', 'darkred', 'gray']
+    palette = sns.color_palette("magma_r", len(clusters))
+    # parameter that regulates the zorder for the plots, brighter on upper levels
     l = len(clusters)
     for i, c in enumerate(sorted(clusters)):
+        # c is the number of the cluster under evaluation, index of results contains the curve numbers
         cluster_index = result[result==c].index.astype(float)
-        # print(cluster_index)
         print(i, "Cluster number %d has %d elements" % (c, len(cluster_index)))
-        # print(type(cluster_index))
-        # print(xaxis.iloc[:, cluster_index].columns)
-        # print(timeSeries.iloc[:, cluster_index].columns)
+        # plotting all the curves whose name (number) is in cluster_index
         ax1[i].plot(xaxis.iloc[:, cluster_index].dropna(axis=1),
                     yaxis.iloc[:, cluster_index].dropna(axis=1), alpha=0.5)
         ax1[i].set_title(('Cluster number '+str(c)), fontsize=15, fontweight='bold')
         ax1[i].set_ylim(-200, 12100)
         ax1[i].set_xlim(0, 550)
+        # plotting all the curves in the same plot differentiated by color
         ax2.plot(xaxis.iloc[:, cluster_index].dropna(axis=1), yaxis.iloc[:, cluster_index].dropna(axis=1),
                  c=palette[i], alpha=0.5, zorder=l)
         ax2.set_ylim(-200, 12100)
@@ -57,21 +53,15 @@ df = pd.read_csv("ROI_ForceCurvesSpline.csv", sep=',')
 df = df.drop('Unnamed: 0', axis=1)
 df_xaxis = pd.read_csv("ROI_ForceCurvesSpline_xaxis.csv", sep=',')
 df_xaxis = df_xaxis.drop('Unnamed: 0', axis=1)
-# print("DF", df.columns)
-# print("X_axis", df_xaxis.columns)
-# fig, ax = plt.subplots()
-# for i in range(df.shape[1]):
-#     ax.plot(df.index, df.iloc[:, i])
-# plt.show()
 
 # run the clustering
-#D = hac.linkage(timeSeries, method='single', metric='correlation')
-# D = hac.linkage(df, method='ward', metric='euclidean')
+#D = linkage(df.T, method='single', metric='correlation')
+# D = linkage(df, method='ward', metric='euclidean')
 # print("Linkage matrix: ", D)
-# plot_dendogram(D)
+# plot_dendrogram(D)
 
-#---- evaluate the dendogram
-# cut_off_level = .5e6# level where to cut off the dendogram
+#---- evaluate the dendrogram
+# cut_off_level = .5e6# level where to cut off the dendrogram
 # plot_results(df.T, df_xaxis.T, D, cut_off_level)
 
 
@@ -86,7 +76,7 @@ num_curves = df.shape[1]
 dtw_distances = np.zeros((num_curves, num_curves))
 
 
-print(dtw_distances)
+# print(dtw_distances)
 start_time = time.time()
 for i in range(num_curves):
     x = df.iloc[:, i].dropna().values
@@ -97,16 +87,9 @@ for i in range(num_curves):
 end_time = time.time()
 print('Processing time: ', end_time - start_time, " sec")
 condensed_distances = squareform(dtw_distances)
+
+# Saving the condensed distance matrix as .csv for future use
 np.savetxt('condensed_distances.csv', condensed_distances, delimiter=',')
 
 
 
-# # Perform hierarchical clustering
-condensed_distances = np.loadtxt('condensed_distances.csv', delimiter=',')
-Z = linkage(condensed_distances, method='ward', metric='euclidean')  # You can choose different linkage methods
-plot_dendogram(Z)
-
-#---- evaluate the dendogram
-# cut_off_level = .5e6# level where to cut off the dendogram
-cut_off_level = input("insert cut off level: ")
-plot_results(df, df_xaxis, Z, cut_off_level)
